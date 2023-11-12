c72c8fa552a3fdccb49c5a735f1a3cd3b74b46a6
Travis <travis.m.bruce@gmail.com>
2023-11-11 20:41:53 -0800

    Basic functional implementation of Rust binary for ios

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {MainStackNavigator} from './navigation/AppNavigator';
-import RustModule from './rust_module/rust_module';
+import RustModule from './rust_module';
 
 import {store} from './redux/store';
 import {Provider} from 'react-redux';
 
 const App = () => {
-  async function add() {
-    let result = await RustModule.add_numbers(1, 2);
-    return result;
-  }
-  console.log(add());
+  RustModule.addNumbers(1, 2).then((result: number) => {
+    console.log(result);
+  });
+  //   let result = await RustModule.addNumbers(1, 2);
+  //   return result;
+  // }
+  // console.log(add());
   return (
     <Provider store={store}>
       <NavigationContainer>
