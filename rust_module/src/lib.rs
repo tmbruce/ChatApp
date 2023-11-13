4979facd7c9730c26a1d713f5ac6be75cf147ab1
Travis <travis.m.bruce@gmail.com>
2023-11-13 11:56:46 -0800

    Restore deleted files

--- rust_module/src/lib.rs
+++ rust_module/src/lib.rs
@@ -1,4 +1,19 @@
+// use jni::objects::{JClass, JString};
+use jni::objects::JClass;
+use jni::sys::jint;
+use jni::JNIEnv;
+
 #[no_mangle]
 pub extern "C" fn add_numbers(a: i32, b: i32) -> i32 {
     a + b
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn Java_com_reactnativepro_RustModule_nativeAddNumbers(
+    _env: JNIEnv,
+    _class: JClass,
+    a: jint,
+    b: jint,
+) -> jint {
+    add_numbers(a, b)
 }
