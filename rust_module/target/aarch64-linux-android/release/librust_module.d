f2833a2ae487dcbf6cc9e08c010fb30af26439c8
Travis <travis.m.bruce@gmail.com>
2023-11-13 18:52:17 -0800

    Streamline android build script

---
+++ rust_module/target/aarch64-linux-android/release/librust_module.d
@@ -0,0 +1,1 @@
+/Users/travis/Documents/Repos/chat-app/ChatApp/rust_module/target/aarch64-linux-android/release/librust_module.a: /Users/travis/Documents/Repos/chat-app/ChatApp/rust_module/src/lib.rs
--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -1,35 +1,23 @@
 #!/bin/zsh
 
 BLUE='\033[1;34m'
+
 RN_ANDROID_DIR='./android/src/main'
 RUST_DIR='./rust_module'
 
-ARCHS="aarch64-linux-android armv7-linux-androideabi"
-ARCH_FOLDERS="arm64-v8a armeabi-v7a"
+ARCHS=("aarch64-linux-android" "armv7-linux-androideabi")
+ARCH_FOLDERS=("arm64-v8a" "armeabi-v7a")
 
-cd $RUST_DIR
-for arch in $ARCHS
-    do 
-        echo "${BLUE}Building for $arch..."
-        cargo ndk build --release --target $arch
+for index in {1..${#ARCHS[@]}}; do
+    arch=${ARCHS[$index]}
+    arch_folder=${ARCH_FOLDERS[$index]}
+    echo "${BLUE}Building for $arch..."
+    cd $RUST_DIR
+    cargo ndk build --release --target $arch
+    cd ..
+    rm -rf $RN_ANDROID_DIR/jniLibs/$arch_folder
+    mkdir -p $RN_ANDROID_DIR/jniLibs/$arch_folder
+    echo "${BLUE}Copying native modules to Android project..."
+    cp $RUST_DIR/target/$arch/release/librust_module.a $RN_ANDROID_DIR/jniLibs/$arch_folder/librust_module.a
+    rm -rf $RUST_DIR/target/$arch
 done
-cd ..
-
-
-# echo "${BLUE}Compiling native modules for Android..."
-# cargo ndk build --release  --target aarch64-linux-android 
-# cargo ndk build --release  --target armv7-linux-androideabi
-# cd ..
-
-rm -rf $RN_ANDROID_DIR/jniLibs/arm64-v8a
-rm -rf $RN_ANDROID_DIR/jniLibs/armeabi-v7a
-
-mkdir -p $RN_ANDROID_DIR/jniLibs/arm64-v8a
-mkdir -p $RN_ANDROID_DIR/jniLibs/armeabi-v7a
-
-echo "${BLUE}Copying native modules to Android project..."
-cp $RUST_DIR/target/aarch64-linux-android/release/librust_module.a $RN_ANDROID_DIR/jniLibs/arm64-v8a/librust_module.a
-cp $RUST_DIR/target/armv7-linux-androideabi/release/librust_module.a $RN_ANDROID_DIR/jniLibs/armeabi-v7a/librust_module.a
-
-rm -rf $RUST_DIR/target/aarch64-linux-android
-rm -rf $RUST_DIR/target/armv7-linux-androideabi
